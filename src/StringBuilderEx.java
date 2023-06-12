a1a7e600663aeb5f1f0918852c21c5765703e643
pinaki1010 <mjpinaki@gmail.com>
2023-06-12 23:44:55 +0530

    Commit For Last day- 12.06.23

--- src/StringBuilderEx.java
+++ src/StringBuilderEx.java
@@ -9,12 +9,25 @@ public class StringBuilderEx {
         printInfomation(name);
         System.out.println("String Builder");
         printInfomation(sb.toString());
-
+        //String Builder CAPACITY
+        System.out.println(sb.capacity());
+        StringBuilder sb1= new StringBuilder(50);//CAPACITY iINCREASES
+        System.out.println(sb1.capacity());
+        //DeleteCharAt
+        sb.deleteCharAt(5).insert(5,"P");
+        printInfomation(sb.toString());
+        //Replace Method
+        sb.replace(0,5,"Sattwiki");
+        printInfomation(sb.toString());
+        //Reverse Method
+        sb.reverse().setLength(12);//Reverse Method and Trim to be String till 7 index
+        printInfomation(sb.toString());
     }
     public static void printInfomation(String name){
 
         System.out.println("Length of String ---->"+name.length());
         System.out.println("String name--->"+name);
+
 
     }
 }
