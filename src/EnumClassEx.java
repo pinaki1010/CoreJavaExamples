86687bebbe16d8624a8263351c235ad042caf77d
pinaki1010 <mjpinaki@gmail.com>
2023-06-14 02:44:26 +0530

    Commit For Enum class- 14.06.23

---
+++ src/EnumClassEx.java
@@ -0,0 +1,12 @@
+public class EnumClassEx {
+    public static void main(String[] args) {
+
+
+        //Enum Example
+
+            WeekOfDayEnumEx weekDay = WeekOfDayEnumEx.FRI;
+            System.out.println("Name---->"+weekDay.name()+"--->Position--->"+weekDay.ordinal());
+
+
+    }
+}
---
+++ src/WeekOfDayEnumEx.java
@@ -0,0 +1,3 @@
+public enum WeekOfDayEnumEx {
+    SUN,MON,TUE,WED,THU,FRI,SAT
+}
