ace637962f9ad30470b3294a44bf643ef5d1a453
pinaki1010 <mjpinaki@gmail.com>
2023-06-15 19:51:51 +0530

    Commit For Interface class- 15.06.23

---
+++ src/InterfaceExampleEx.java
@@ -0,0 +1,66 @@
+//Interface Example Human
+interface Human{
+    void eat();
+    void drink();
+    void sleep();
+
+}
+
+//Child class Implements Human
+class Men implements Human{
+
+    @Override
+    public void eat() {
+        System.out.println("Men Eating");
+    }
+
+    @Override
+    public void drink() {
+        System.out.println("Men Drinking");
+    }
+
+    @Override
+    public void sleep() {
+
+        System.out.println("Men Sleeping");
+
+    }
+}
+//Child class Implements Human
+class Women implements Human{
+
+    @Override
+    public void eat() {
+        System.out.println("WoMen Eating");
+    }
+
+    @Override
+    public void drink() {
+        System.out.println("WoMen Drinking");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("WoMen Sleeping");
+    }
+}
+
+
+
+
+public class InterfaceExampleEx {
+    public static void main(String[] args) {
+
+        Men men=new Men();
+        men.eat();
+        men.drink();
+        men.sleep();
+
+        Human women = new Women();
+
+        women.eat();
+        women.drink();
+        women.sleep();
+
+    }
+}
